bb01d361c998b39ea9b51a13d40535bf62953a19
wuyiqiang <wu_yiqiang@outlook.com>
2020-11-19 11:50:37 +0800

    2020-11-19

--- js/login.js
+++ js/login.js
@@ -17,28 +17,25 @@ function check_login() {
     var final = reg.test(emial_info)
     if (final != true) {
         // 匹配失败时，生成提示信息
-
+        
         //先将其他提示信息向下移动80px,再添加提示信息
         //遍历所有类名为woring的html，然后将woring的定位修改为原来的top值+80px
-        var woringclass = document.getElementsByClassName("woring")
-        woringclass.style.top=`(parseInt(woringclass.style.top)+80+"px")`
+        //获取所有报警节点的top值，将自身的top值再加上80px，再赋值
+        /*$('.woring').each( function (indexInArray, valueOfElement) {
+            var topval = this.top
+            $('div.woring').css('top','+=80')
 
+        });*/
         //在页面的右上角生成报警窗口
-        var woring_info=`<div class="woring">
-                            <span class="iconfont icon-anquan1"></span>
-                            <div>
-                                <h3>登录失败</h3>
-                                <h4>用户名或密码错误</h4>
-                            </div>
-                        </div >`
-        document.getElementById("tops").insertBefore(woring_info,null)
-        alert("匹配失败")
-    } else {
-        // 匹配成功时，将数据发送至服务器，再接受由服务器发送到客户端的文件
+        var newdata = $('<div><span class="iconfont icon-anquan1"></span><div><h3>登录失败</h3><h4>用户名或密码错误</h4></div></div>')
+        //获取顶端页面
+        $('.top_part').prepend('+')
         
+
+        alert($('div.topbar').before(newdata))
+    } else {
+        // 匹配成功时，将数据发送至服务器，再接受由服务器发送到客户端的文件   
     }
-
-
    /*匹配从@开始到结束的字符*/
  /*  var patten_list=user_email.splice(email_info.indexOf("@"),user_email.length-1)
    for(let i=0;i<email_list.length;i++){
@@ -49,16 +46,18 @@ function check_login() {
        }
     }
 */
-
-
     /*匹配失败,生成报警框*/
-
-
-
-
-
     /*三秒之后从下往上依次清除报警框*/
-
-
-
 }
+
+//获取button元素
+var getbutton = document.getElementById('buttons')
+//适配版本IE8以下的浏览器
+if (getbutton.addEventListener){
+    getbutton.addEventListener('click',check_login,false)
+
+}else {
+    getbutton.attachEvent('onclick',check_login,false)
+}
+
+
